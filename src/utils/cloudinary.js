7f1effd1ef26d1b2a6c9edb21bc60a3de6a44245
CodeeHammad <hammadullahjan1@gmail.com>
2024-03-24 00:43:43 +0500

    error free code until know

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -15,11 +15,12 @@ const uploadFileOnCloudinary = async (localFilePath)=>{
         })  
         //file has been uploaded 
 
-        console.log("file has been uploaded on cloudinary" , response.url)
+        // console.log("file has been uploaded on cloudinary" , response.url)
+        fs.unlinkSync(localFilePath)
         return response
     } catch (error) {
         fs.unlinkSync(localFilePath) // removed  temperory files after the operation 
-
+        return null
     }
 }
 
