505b4dc6e95887f886c3b7ef9332de902755e7c5
CodeeHammad <hammadullahjan1@gmail.com>
2024-03-23 22:38:27 +0500

    configure validation in user.controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,5 +1,5 @@
 import {asyncHandler} from '../utils/asyncHandler.js'
-
+import {ApiError} from '../utils/ApiError.js'
 
 const registerUser = asyncHandler( async (req, res) =>{
     //get user details from frontend    
@@ -11,6 +11,16 @@ const registerUser = asyncHandler( async (req, res) =>{
     //remove password and refresh token field from response 
     // check for user creation
     //return response
+
+    const {fullname , email , username , password } = req.body
+    console.log("email" , email)
+
+    if ([
+        fullname , email , username , password].some((field)=> field?.trim() === 
+        "")
+    ) {
+        throw new ApiError(400 , "All fields are required")
+    }
 
 })
 
