ed3d20b45efc42e81d5eb2ed79b83a981d895722
CodeeHammad <hammadullahjan1@gmail.com>
2024-03-25 15:02:12 +0500

    finally the user controller is finished and bugs free

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -16,7 +16,7 @@ const registerUser = asyncHandler( async (req, res) =>{
     //return response
 
     const {fullname , email , username , password } = req.body
-    console.log("email" , email)
+    // console.log("email" , email)
 
     if ([
         fullname , email , username , password].some((field)=> field?.trim() === 
@@ -32,6 +32,7 @@ const registerUser = asyncHandler( async (req, res) =>{
         }
     const avatarLocalPath = req.files?.avatar[0]?.path;
     let coverImageLocalPath ; 
+
     if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
         coverImageLocalPath = req.files.coverImage[0].path
     }
