a2686ca7fd889e33aa7f5b98003d8b2383d0ed97
CodeeHammad <hammadullahjan1@gmail.com>
2024-04-17 21:42:02 +0500

    .

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -95,7 +95,7 @@ const loginUser = asyncHandler( async (req ,res)=>{
         // password validation  
         // access & refresh token generation
         const {email , username , password} = req.body
-        if(!username || !email){
+        if(!(username || email)){
             throw new ApiError(400 , "username or email is required")
         }
         const user = await User.findOne({
