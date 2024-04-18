33003078804efa7bdc4ba7e765b73985b03fe546
CodeeHammad <hammadullahjan1@gmail.com>
2024-04-18 23:18:54 +0500

    fix bugs

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -227,7 +227,7 @@ const changeUserPassword = asyncHandler(async (req , res)=>{
 
 const getCurrentUser = asyncHandler(async (req ,res)=>{
     return res.status(200)
-    .json(200 , req.user , "current user fetched successfully") 
+    .json(new ApiResponse(200 , req.user , "current user fetched successfully") )
 })
 
 const updateAccountDetails = asyncHandler(async (req , res)=>{
@@ -238,7 +238,7 @@ const updateAccountDetails = asyncHandler(async (req , res)=>{
     }
 
     //finding user and upadating user details 
-    const user = User.findByIdAndUpdate(req.user?_id ,
+    const user = await User.findByIdAndUpdate(req.user?_id ,
          {$set: {
         fullname : fullname ,
         email : email
