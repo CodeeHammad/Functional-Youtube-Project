eda193bc6399c75872d76c38a83e3720cbb43d15
CodeeHammad <hammadullahjan1@gmail.com>
2024-04-22 18:04:32 +0500

    added controller function for getting video by id

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -67,11 +67,36 @@ const publishAVideo = asyncHandler(async (req ,res)=>{
 const getVideoById = asyncHandler(async (req, res) => {
     const { videoId } = req.params
     //TODO: get video by id
+
+    if (!videoId) {
+        throw new ApiError(401 , "video id is required")
+    }
+
+
+
+    const video = await Video.findById(videoId)
+    const videourl = video.url
+    
+    if(!video){
+        throw new ApiError(401 , "video is not available")
+    }
+    return res.status(200)
+    .json(
+        new ApiResponse(200 , videourl , " video has been uploaded")
+    )
+
 })
+
+
+
+
+
 
 const updateVideo = asyncHandler(async (req, res) => {
     const { videoId } = req.params
     //TODO: update video details like title, description, thumbnail
+    
+
 
 })
 
--- src/routes/video.routes.js
+++ src/routes/video.routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { publishAVideo } from "../controllers/video.controller.js";
+import { getVideoById, publishAVideo } from "../controllers/video.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router()
+router.use(verifyJWT)
 router.route("/publish-video").post(
     upload.fields(
         [{
@@ -17,5 +18,10 @@ router.route("/publish-video").post(
     )
     , publishAVideo
 )
+router.route("/:videoId").get(getVideoById)
 
 export default router
+
+// {
+//     "videoId" : "662412317e94f056dc893f32"
+// }
